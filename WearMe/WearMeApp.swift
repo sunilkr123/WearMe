6aed9dd2bc57fc95cf78a34bfaf66b570bd94357
Abhishek Thakur <abhishek.thakur@techalchemy.co>
2024-02-07 14:16:30 +0530

    Initial Commit

---
+++ WearMe/WearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearMeApp.swift
+//  WearMe
+//
+//  Created by macbook27 on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
